901b5dc7fd18688a9901a1ba7732b20f90b19f5e
Muhammed Melih Yurt <yurtmmy@gmail.com>
2021-04-22 01:45:57 +0300

    Changes on API

--- api/index.js
+++ api/index.js
@@ -1,29 +1,21 @@
 /*
  * /index.js
-*/
+ */
 
 const express = require("express");
 const app = express();
 const router = require("./routes");
 
-require('dotenv').config();
-const port = process.env.PORT
-const host = process.env.HOST
-
-//app.use(cors({ origin: true }));     
-
-app.use(function (req, res, next) {                     //To handle CORS policy error
+app.use(function (req, res, next) {
+  //To handle CORS policy error
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 
+app.use(express.urlencoded({ extended: true })); //Body Parser
+app.use("/", router); //Router Middleware
 
-app.use(express.urlencoded());    //Body Parser
-app.use('/', router);     //Router Middleware
-
-
-
-app.listen(port, () => {
-  console.log(`API listening on ${host}:${port}`);
+app.listen(5000, () => {
+  console.log(`API listening on localhost:5000`);
 });
